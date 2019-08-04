4b0a92ab3a1033131703ddbc07facba82ebb9b34
adamlamrini <adamhassan776@gmail.com>
2019-08-04 14:50:00 +0000

    final changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'e10056ba40a64365a1faac9389243fad.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'e10056ba40a64365a1faac9389243fad.vfs.cloud9.us-east-1.amazonaws.com', ('mrboost-ecommerce.herokuapp.com')]
 
 
 # Application definition
